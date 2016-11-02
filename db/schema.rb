819a74f6a69e757ba55125277fb54f8e278c992c
vinicomelo <vinicomelo@gmail.com>
2016-11-02 13:39:33 -0400

    added images to the show and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102165717) do
+ActiveRecord::Schema.define(version: 20161102171632) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,9 +39,13 @@ ActiveRecord::Schema.define(version: 20161102165717) do
     t.text     "finish"
     t.text     "overall"
     t.integer  "score"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
